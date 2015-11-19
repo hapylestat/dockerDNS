b46954fbe897ce1ddd5f30d611ff26d6cd1ea076
Reishin <hapylestat@users.noreply.github.com>
2015-11-19 14:43:14 +0200

    cut obsolete code

--- docker_dns.py
+++ docker_dns.py
@@ -8,35 +8,6 @@ from dnslite.types import QuestionItem
 from dnslite.datapack import ip_to_in_addr
 
 
-
-
-class DNSQuery:
-  def __init__(self, data):
-    self.data = data
-    self.dominio = ''
-
-    tipo = (data[2] >> 3) & 15   # Opcode bits
-    if tipo == 0:                # Standard query
-      ini = 12
-      lon = data[ini]
-      while lon != 0:
-        self.dominio += data[ini + 1:ini + lon + 1].decode() + '.'
-        ini += lon + 1
-        lon = data[ini]
-
-  def response(self, ip):
-    byte_ip = bytearray(map(lambda x: int(x), ip.split('.')))
-
-    packet = b''
-    if self.dominio:
-      packet += self.data[:2] + b"\x81\x80"
-      packet += self.data[4:6] + self.data[4:6] + b'\x00\x00\x00\x00'     # Questions and Answers Counts
-      packet += self.data[12:]                                           # Original Domain Name Question
-      packet += b'\xc0\x0c'                                               # Pointer to domain name
-      packet += b'\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'               # Response type, ttl and resource data length -> 4 bytes
-      packet += byte_ip  # 4bytes of IP
-    return packet
-
 ip_cache = {}
 
 
@@ -69,7 +40,6 @@ if __name__ == '__main__':
       for item in m.question_section:
         print(item)
 
-      p = DNSQuery(data)
       q = m.get_question(0)
 
       RET_CODE = RCODE.NO_ERROR
